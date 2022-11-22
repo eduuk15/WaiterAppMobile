0b295a36e9a861182e5b8d8dbc7afc16b38639c0
eduuk15 <98836125+eduuk15@users.noreply.github.com>
2022-11-22 18:00:12 -0300

    feat: adiciona componente reutilizavel do botao

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -3,6 +3,7 @@ import { Container, CategoriesContainer, MenuContainer, Footer, FooterContainer 
 import { Header } from '../components/Header';
 import { Categories } from '../components/Categories';
 import { Menu } from '../components/Menu';
+import { Button } from '../components/Button';
 
 export function Main() {
 	return (
@@ -20,7 +21,11 @@ export function Main() {
 
 			</Container>
 			<Footer>
-				<FooterContainer></FooterContainer>
+				<FooterContainer>
+					<Button onPress={() => alert('Novo Pedido')}>
+						Novo Pedido
+					</Button>
+				</FooterContainer>
 			</Footer>
 		</>
 	);
