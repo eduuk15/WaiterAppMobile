1e0bb5237d06f7c5df11002db4f071c85e9130f7
eduuk15 <98836125+eduuk15@users.noreply.github.com>
2022-11-24 14:43:30 -0300

    feat: adiciona componente de modal do pedido, configurando seus comportamentos

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,11 +1,21 @@
 import { Container, CategoriesContainer, MenuContainer, Footer, FooterContainer } from './styles';
+import { useState } from 'react';
 
 import { Header } from '../components/Header';
 import { Categories } from '../components/Categories';
 import { Menu } from '../components/Menu';
 import { Button } from '../components/Button';
+import { TableModal } from '../components/TableModal';
 
 export function Main() {
+	const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+	const [selectedTable, setSelectedTable] = useState('');
+
+	function handleSaveTable(table: string) {
+		setSelectedTable(table);
+		setIsTableModalVisible(false);
+	}
+
 	return (
 		<>
 			<Container>
@@ -22,11 +32,19 @@ export function Main() {
 			</Container>
 			<Footer>
 				<FooterContainer>
-					<Button onPress={() => alert('Novo Pedido')}>
+					{!selectedTable && (
+						<Button onPress={() => setIsTableModalVisible(true)}>
 						Novo Pedido
-					</Button>
+						</Button>
+					)}
 				</FooterContainer>
 			</Footer>
+
+			<TableModal
+				visible={isTableModalVisible}
+				onClose={() => setIsTableModalVisible(false)}
+				onSave={handleSaveTable}
+			/>
 		</>
 	);
 }
