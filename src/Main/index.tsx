44902a8b6bb61ce5a6f708bad49d2f31e0523b7f
eduuk15 <98836125+eduuk15@users.noreply.github.com>
2022-12-13 14:12:42 -0300

    feat: adiciona status de carregamento quando informacoes demoram pra vir da api

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,5 +1,6 @@
 import { Container, CategoriesContainer, MenuContainer, Footer, FooterContainer, CenteredContainer } from './styles';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { api } from '../utils/api';
 
 import { Header } from '../components/Header';
 import { Categories } from '../components/Categories';
@@ -11,17 +12,40 @@ import { CartItem } from '../types/CartItem';
 import { Product } from '../types/Product';
 import { ActivityIndicator } from 'react-native';
 
-import { products as mockProducts } from '../mocks/products';
 import { Empty } from '../components/Icons/Empty';
 import { Text } from '../components/Text';
-
+import { Category } from '../types/Category';
 
 export function Main() {
 	const [isTableModalVisible, setIsTableModalVisible] = useState(false);
 	const [selectedTable, setSelectedTable] = useState('');
 	const [cartItems, setCartItems] = useState<CartItem[]>([]);
-	const [isLoading] = useState(false);
-	const [products] = useState<Product[]>(mockProducts);
+	const [isLoading, setIsLoading] = useState(true);
+	const [products, setProducts] = useState<Product[]>([]);
+	const [categories, setCategories] = useState<Category[]>([]);
+	const [isLoadingProducts, setIsLoadingProducts] = useState(false);
+
+	useEffect(() => {
+		Promise.all([
+			api.get('/categories'),
+			api.get('/products')
+		]).then(([categoriesResponse, productsResponse]) => {
+			setCategories(categoriesResponse.data);
+			setProducts(productsResponse.data);
+			setIsLoading(false);
+		});
+	}, []);
+
+	async function handleSelectCategory(categoryId: string) {
+		const route = !categoryId ? '/products' : `/categories/${categoryId}/products`;
+
+		setIsLoadingProducts(true);
+
+		const { data } = await api.get(route);
+
+		setProducts(data);
+		setIsLoadingProducts(false);
+	}
 
 	function handleSaveTable(table: string) {
 		setSelectedTable(table);
@@ -101,23 +125,34 @@ export function Main() {
 				) : (
 					<>
 						<CategoriesContainer>
-							<Categories></Categories>
+							<Categories
+								onSelectCategory={handleSelectCategory}
+								categories={categories}
+							></Categories>
 						</CategoriesContainer>
 
-						{products.length > 0 ? (
-							<MenuContainer>
-								<Menu
-									onAddToCart={handleAddToCart}
-									products={products}
-								/>
-							</MenuContainer>
-						) : (
+						{isLoadingProducts ? (
 							<CenteredContainer>
-								<Empty />
-								<Text color='#666' style={{ marginTop: 24 }}>
-									Nenhum produto foi encontrado!
-								</Text>
+								<ActivityIndicator color="#D73035" size="large"/>
 							</CenteredContainer>
+						) : (
+							<>
+								{products.length > 0 ? (
+									<MenuContainer>
+										<Menu
+											onAddToCart={handleAddToCart}
+											products={products}
+										/>
+									</MenuContainer>
+								) : (
+									<CenteredContainer>
+										<Empty />
+										<Text color='#666' style={{ marginTop: 24 }}>
+										Nenhum produto foi encontrado!
+										</Text>
+									</CenteredContainer>
+								)}
+							</>
 						)}
 					</>
 				)}
--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,16 +1,22 @@
 import { FlatList } from 'react-native';
 import { useState } from 'react';
 
-import { categories } from '../../mocks/categories';
 import { Text } from '../Text';
 
-import { Category, Icon } from './styles';
+import { CategoryContainer, Icon } from './styles';
+import { Category } from '../../types/Category';
 
-export function Categories() {
+interface CategoriesProps {
+	categories: Category[];
+	onSelectCategory: (categoryId: string) => Promise<void>;
+}
+
+export function Categories({ categories, onSelectCategory }: CategoriesProps) {
 	const [selectedCategory, setSelectedCategory] = useState('');
 
 	function handleSelectCategory(categoryId: string) {
 		const category = selectedCategory == categoryId ? '' : categoryId;
+		onSelectCategory(category);
 		setSelectedCategory(category);
 	}
 
@@ -25,7 +31,7 @@ export function Categories() {
 				const isSelected = selectedCategory == category._id;
 
 				return (
-					<Category onPress={() => handleSelectCategory(category._id)}>
+					<CategoryContainer onPress={() => handleSelectCategory(category._id)}>
 						<Icon>
 							<Text opacity={isSelected ? 1 : 0.5}>
 								{category.icon}
@@ -35,7 +41,7 @@ export function Categories() {
 						<Text size={14} weight="600" opacity={isSelected ? 1 : 0.5}>
 							{category.name}
 						</Text>
-					</Category>
+					</CategoryContainer>
 				);
 			}}
 		/>
